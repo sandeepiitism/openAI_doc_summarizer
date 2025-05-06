327b7877700187a67a1c59fe83ba3ebcf4c31bfc
Sandeep <sandeepiit.ism@gmail.com>
2025-05-06 15:30:36 +0530

    notebook tested

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Document Summarizer using OpenAI - GenAI project",
+    version= '0.0.1',
+    author="Sandeep Chowdhury",
+    author_email="sandeepiit.ism@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
